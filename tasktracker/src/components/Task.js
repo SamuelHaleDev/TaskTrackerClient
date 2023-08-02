3bac97ee11c825cec3474c3a62fb0b9e9c89df84
ww-samuel-hale <samuel.hale@woodwing.com>
2023-08-02 17:19:35 -0400

    Made changes to TaskList

--- tasktracker/src/components/Task.js
+++ tasktracker/src/components/Task.js
@@ -3,15 +3,29 @@ import React from 'react';
 import './css/Task.css';
 
 const Task = ({ task }) => {
+  const onClick = e => {
+    var coll = document.getElementsByClassName("collapsible");
+    var i;
+
+    for (i = 0; i < coll.length; i++) {
+      coll[i].classList.toggle("active");
+      var content = coll[i].nextElementSibling;
+      if (content.style.display === "block") {
+        content.style.display = "none";
+      } else {
+        content.style.display = "block";
+      }
+    }
+  }
 
   return (
     <>
       <li className='Task'>
-        <a href="#">
-          <span>Task: {task.title}</span>
+        <button type="button" className='collapsible' onClick={onClick}>Task: {task.title}</button>
+        <div className="content">
           <span>Description: {task.description}</span>
           <span>Due: {task.deadline}</span>
-        </a>
+        </div>
       </li>
     </>
   )
